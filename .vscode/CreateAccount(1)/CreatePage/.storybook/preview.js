caf6731793f650d2e4e580f9de3b471bb998f701
Johannah Ruiz <131231470+mojojoruiz@users.noreply.github.com>
2024-02-07 11:54:01 -0700

    Create Account
    I'm still working on it but this is what I have so far

---
+++ .vscode/CreateAccount(1)/CreatePage/.storybook/preview.js
@@ -0,0 +1,17 @@
+const preview = {
+    parameters:{
+        actions:{argTypesRegex:"^on[A-Z].*" },
+        controls:{
+            matchers:{
+                color:/(background|color)$/i,
+                date:/Date$/,
+            },
+        },
+    
+    },
+
+};
+
+export const decorators = [(Story,context)=> <Story key ={JSON.stringify(context.args)} /> ];
+export default preview;
+
---
+++ .vscode/CreateAccount(1)/CreatePage/src/screens/MacbookPro/MacbookPro.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookPro= () => {
+    return (
+        <div className="macbook-pro">
+            <div className="element-wrapper">
+                <div className="element">
+                    <div className="overlap-group">
+                    <img
+                    className="img" 
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/vector.svg"
+                    />
+
+                    <img
+                    className="img" 
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/vector.svg"
+                    />
+
+                    <div className="group">
+                    <img
+                    className="img"
+                    alt="Rectangle"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/rectangle.png"
+                    />
+
+                    <img
+                    className="clip-path-group"
+                    alt="Clip path group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/clip-path-group.png"
+                    />
+
+                    <img
+                    className="clip-path-group-2"
+                    alt="Clip path group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/clip-path-group-1@2x.png"
+                    />
+
+                    <img
+                    className="group-2" 
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group@2x.png"
+                    />
+
+                    <img
+                    className="group-3"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-1@2x.png"
+                    />
+                    <img
+                    className="group-4"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-2@2x.png"
+                    />
+                    <img
+                    className="group-5"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-3@2x.png"
+                    />
+                    <img
+                    className="group-6"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-4@2x.png"
+                    />
+                    <img
+                    className="group-7"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-5@2x.png"
+                    />
+                    <img
+                    className="group-8"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-6@2x.png"
+                    />
+                    <img
+                    className="group-9"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-7@2x.png"
+                    /> 
+                    <img
+                    className="group-10"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-8@2x.png"
+                    /> 
+                    <img
+                    className="group-11"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-9@2x.png"
+                    /> 
+                    <img
+                    className="group-12" alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-10@2x.png"
+                    /> 
+                    <img
+                    className="group-13"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-11@2x.png"
+                    /> 
+                    <img
+                    className="group-14"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-12@2x.png"
+                    />
+                    <img
+                    className="group-15"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-13@2x.png"
+                    />
+                    <img
+                    className="group-16"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-14@2x.png"
+                    />
+                    <img
+                    className="vector"
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd84/releases/65c3046ab22a476955a95ecb/img/vector-2.svg"
+                    />
+                    <img
+                    className="clip-path-group-3" alt="Clip path group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/clip-path-group-2@2x.png"
+                    />
+                    <img
+                    className="vector-2"
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd84/releases/65c3046ab22a476955a95ecb/img/vector-3.svg"
+                    /> 
+                    <img
+                    className="group-17"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-15@2x.png"
+                    />
+                    <img
+                    className="group-18"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-16@2x.png"
+                    /> 
+                    <img
+                    className="group-19" alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-17@2x.png"
+                    />
+                    <img
+                    className="group-20"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-18@2x.png"
+                    /> 
+                    <img
+                    className="group-21"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-19@2x.png"
+                    />
+                    <img
+                    className="group-22"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-20@2x.png"
+                    /> 
+                    <img
+                    className="group-23"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65c30421b77185cd73cafd04/releases/65c3046ab22a476955a95ecb/img/group-21@2x.png"
+                    />
+                    
+
+
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
