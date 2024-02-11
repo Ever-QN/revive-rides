bac24430d3f27762487b7830325c836840a5b464
Johannah Ruiz <131231470+mojojoruiz@users.noreply.github.com>
2024-02-10 21:46:46 -0700

    pls work :3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [
-      require('@daisyui/core'),
-    ],
-  };
-  
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    require('daisyui'),
+  ],
+}
