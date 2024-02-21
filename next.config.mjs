c8211307217bf5d8c6ed8c1bb30389345b7e565d
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2024-02-21 14:15:04 -0700

    Change initial redirect to /home

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const redirects = [
+{
+    source: '/app/home',
+    destination: '/home',
+    permanent: true,
+},
+];
+
+const nextConfig = {
+async redirects() {
+    return redirects;
+},
+};
 
 export default nextConfig;
