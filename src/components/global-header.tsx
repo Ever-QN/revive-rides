0154bead9f984c976cbc9aa87099963a57aad821
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-21 18:01:29 -0700

    Remove comment

--- src/components/global-header.tsx
+++ src/components/global-header.tsx
@@ -1,8 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-/**
- * @see https://v0.dev/t/Dterm7GA3RV
- */
-
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { PopoverTrigger, Popover, PopoverContent } from "@/components/ui/popover"
