fd8a55ec1ae980c80565cb408fa98ad7324584c0
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-27 22:56:29 -0700

    Remove unnecessary div

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -133,7 +133,6 @@ export default function Component() {
       </div>
       <div className="w-full py-12 md:py-24">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-2">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Customer Testimonials</h2>
               <p className="max-w-3xl text-gray-500 md:text-xl/95 dark:text-gray-400">
                 See what our customers have to say about our auto body repair services.
@@ -145,7 +144,6 @@ export default function Component() {
               speed="slow"
               />
             </div>
-          </div>
         </div>
     </div>
       <div className="w-full py-12 md:py-24">
