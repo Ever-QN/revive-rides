4692418687de44b8ee86b394000c4f0850fe0c60
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-28 07:39:10 -0700

    Redirect if in / directory to /home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import Link from 'next/link';
 import type { NextPage } from 'next';
 import './globals.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const AppPage: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/home');
+  }, []);
   return (
     <>
+      <p>Redirecting to Home...</p>
     </>
   );
 };
