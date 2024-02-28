56af81d135295f43e34876bafe0aed1ed35c1dcc
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-28 04:58:23 -0700

    Remove wrench icon SVG and rename some variables

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -44,7 +44,7 @@ const testimonials = [
   },
 ];
 
-const images = [
+const sliderImages = [
   "/images/slider/blackcarsunset.jpg",
   "/images/slider/truck.jpg",
   "/images/slider/brokentruck.jpg",
@@ -55,7 +55,7 @@ export default function Home() {
   return (
     <>
       <div className="w-full pt-24">
-        <ImagesSlider className="h-[50rem]" images={images}>
+        <ImagesSlider className="h-[50rem]" images={sliderImages}>
           <motion.div
             initial={{
               opacity: 0,
@@ -269,24 +269,5 @@ export default function Home() {
         <p>© 2024 S&D Autobody. All rights reserved.</p>
       </footer>
     </>
-  )
-}
-
-function WrenchIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" />
-    </svg>
   )
 }
