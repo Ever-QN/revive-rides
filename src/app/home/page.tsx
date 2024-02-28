e5e01dc42f05482539b92ad3ad59640b6b828fb2
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-28 07:29:13 -0700

    Change ownerImageUrl and add more page information

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -46,12 +46,12 @@ const testimonials = [
 
 const sliderImages = [
   "/images/slider/blackcarsunset.jpg",
+  "/images/slider/whitecar.jpg",
   "/images/slider/truck.jpg",
-  "/images/slider/brokentruck.jpg",
 ];
 
 export default function Home() {
-  const ownerImageUrl = "/images/dalbir-and-sukhbir.jpg";
+  const ownerImageUrl = "/images/sukhbir-and-dalbir.jpg";
   return (
     <>
       <div className="w-full pt-24">
@@ -89,14 +89,18 @@ export default function Home() {
           <div className="flex flex-row-reverse gap-8 items-start">
             <div className="h-[20rem] relative">
               <DirectionAwareHover imageUrl={ownerImageUrl}>
-                <p className="font-bold text-xl">Dalbir and Sukhbir</p>
+                <p className="font-bold text-xl">Sukhbir and Dalbir</p>
                 <p className="font-normal text-sm">Pictured in front of the Golden Temple in Amritsar, Punjab</p>
               </DirectionAwareHover>
           </div>  
           <div className="flex flex-col space-y-2 md:overflow-hidden lg:overflow-hidden sm:overflow-y-scroll max-h-76">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">About Us</h2>
-              <p className="max-w-3xl text-gray-500 md:text-xl/95 dark:text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <p className="max-w-3xl text-gray-700 md:text-xl/95 dark:text-gray-500">
+                Driven by a Vision, S&amp;D Autobody Takes Root <br/><br/>
+
+                Sukhbir and Dalbir Bachhal&apos;s journey started in Victoria, British Columbia, where they witnessed a need for reliable and affordable auto body repair services. This ignited a passion to create a company that not only delivered high-quality vehicle work, but also treated every customer with respect, honesty, and offered competitive prices.<br/><br/>
+
+                Their vision culminated in the birth of S&D Autobody. Beginning as a small shop in Victoria, the company steadily grew, driven by its commitment to exceptional service and customer satisfaction. Recognizing the potential for further impact, S&D Autobody embarked on an expansion journey, establishing a new location in Strathmore, Alberta. This expansion allowed them to reach a wider customer base and continue delivering their exceptional service to an even larger community.
               </p>
           </div>
       </div>
@@ -122,7 +126,7 @@ export default function Home() {
                 />
               </div>
               <h3 className="text-xl font-bold">Auto Body Repair</h3>
-              <p className="text-sm text-gray-500/90">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-sm text-gray-500/90">Fixes such as dents, scratches, cracks, and collisions, restoring its functionality and appearance.</p>
             </div>
             <div className="flex flex-col items-center justify-center space-y-2 text-center">
               <div className="flex items-center justify-center w-24 h-24 rounded-full bg-gray-100/70">
@@ -135,7 +139,7 @@ export default function Home() {
                   />
               </div>
               <h3 className="text-xl font-bold">Paintless Dent Repair</h3>
-              <p className="text-sm text-gray-500/90">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-sm text-gray-500/90">Removes dents from the body using specialized tools and techniques, without the need for traditional bodywork, paint, or fillers.</p>
             </div>
             <div className="flex flex-col items-center justify-center space-y-2 text-center">
               <div className="flex items-center justify-center w-24 h-24 rounded-full bg-gray-100/70">
@@ -148,7 +152,7 @@ export default function Home() {
                   />
               </div>
               <h3 className="text-xl font-bold">Auto Detailing</h3>
-              <p className="text-sm text-gray-500/90">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-sm text-gray-500/90">A thorough cleaning and restoration process. Services typically include washing, waxing, polishing, vacuuming, stain removal, and leather or upholstery care.</p>
             </div>
             <div className="flex flex-col items-center justify-center space-y-2 text-center">
               <div className="flex items-center justify-center w-24 h-24 rounded-full bg-gray-100/70">
@@ -161,7 +165,7 @@ export default function Home() {
                   />
               </div>
               <h3 className="text-xl font-bold">Frame Straightening</h3>
-              <p className="text-sm text-gray-500/90">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-sm text-gray-500/90">Corrects misalignments or bends in your vehicle's chassis (frame) caused by accidents or collisions, ensuring proper wheel alignment and safe driving.</p>
             </div>
             <div className="flex flex-col items-center justify-center space-y-2 text-center">
               <div className="flex items-center justify-center w-24 h-24 rounded-full bg-gray-100/70">
@@ -174,7 +178,7 @@ export default function Home() {
                   />
               </div>
               <h3 className="text-xl font-bold">Graphics and Decals</h3>
-              <p className="text-sm text-gray-500/90">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-sm text-gray-500/90">Apply custom designs, logos, or lettering to your vehicle using vinyl decals or wraps.</p>
             </div>
             <div className="flex flex-col items-center justify-center space-y-2 text-center">
               <div className="flex items-center justify-center w-24 h-24 rounded-full bg-gray-100/70">
@@ -187,7 +191,7 @@ export default function Home() {
                   />
               </div>
               <h3 className="text-xl font-bold">Custom Paint Jobs</h3>
-              <p className="text-sm text-gray-500/90">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-sm text-gray-500/90">Offers complete repainting or unique paint schemes for your vehicle. This can involve solid colors, metallic finishes, intricate designs, or airbrushing for a personalized touch</p>
             </div>
           </div>
         </div>
