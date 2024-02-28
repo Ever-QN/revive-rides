4cb0865e25b39b2f06cb375cfc81b0ebcc7a1aa7
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-28 03:58:50 -0700

    Add shimmer effects for button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,6 +82,14 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        shimmer: {
+          from: {
+            "backgroundPosition": "0 0"
+          },
+          to: {
+            "backgroundPosition": "-200% 0"
+          }
+        },
         scroll: {
           to: {
             transform: "translate(calc(-50% - 0.5rem))",
@@ -97,6 +105,7 @@ const config = {
         },
       },
       animation: {
+        shimmer: "shimmer 2s linear infinite",
         scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
