ef400be2253a096d04c8858cfbbbf502b8f6b009
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-29 00:57:50 -0700

    Change href link to /book-appointment

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
             <motion.p className="text-4xl text-center md:text-4xl text-red-500 bg-clip-text dark:text-gray-300">
               Discover excellence in auto body repair services.
             </motion.p>
-            <Link className="px-4 py-2 border border-red-400 bg-red-700 text-white mx-auto text-center rounded-full relative mt-4 hover:scale-110 active:scale-125 transition-all duration-100" href="/booking">
+            <Link className="px-4 py-2 border border-red-400 bg-red-700 text-white mx-auto text-center rounded-full relative mt-4 hover:scale-110 active:scale-125 transition-all duration-100" href="/book-appointment">
               <span>Book an Appointment →</span>
             </Link>
 
