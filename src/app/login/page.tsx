284e070d5111cb4567221bc447c4302cdfcc0808
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-28 18:04:36 -0700

    Revamp entire login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,127 +1,130 @@
-'use client';
-import React, { useState, FormEvent } from 'react';
-import '/styles/global.css'; // Import global styles if needed
+"use client"
 
-interface LoginFormProps {}
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { BackgroundGradient } from "@/components/ui/background-gradient"
+import Image from "next/image"
+import Link from "next/link"
 
-const LoginForm: React.FC<LoginFormProps> = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [rememberMe, setRememberMe] = useState(false);
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    // Perform login logic here
-    // For example, you could call an API endpoint to authenticate the user
-    console.log('Submitting form with values:', { username, email, password, rememberMe });
-  };
-
+export default function Login() {
   return (
-    <div className='background-image min-h-screen flex items-center justify-center'>
-      <div className='bg-white shadow-md rounded p-8'>
-        <h1 className='text-2xl font-bold mb-4 text-center text-black'>Login</h1>
-        <form onSubmit={handleSubmit}>
-          {/* Username Field */}
-          <div className='mb-4'>
-            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='username'>
-              Username
-            </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
-              type='text'
-              name='username'
-              placeholder='Username'
-              required
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          {/* Email Field */}
-          <div className='mb-4'>
-            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
-              Email
-            </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline'
-              id='email'
-              type='email'
-              name='email'
-              placeholder='Email'
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          {/* Password Field */}
-          <div className='mb-6'>
-            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
-              Password
-            </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline'
-              id='password'
-              type='password'
-              name='password'
-              placeholder='Password'
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {/* Sign in Buttons */}
-          <div className='flex items-center justify-between'>
-            <button
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-              type='submit'
-            >
-              Login
-            </button>
-          </div>
-          {/* Social Sign in Options */}
-          <div className='mt-4'>
-            <button
-              className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full'
-            >
-              Continue with Google
-            </button>
-          </div>
-          <div className='mt-4'>
-            <button
-              className='bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full'
-            >
-              Continue with Apple
-            </button>
-          </div>
-          <div className='mt-4'>
-            <button
-              className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full'
-            >
-              Continue with Phone Number
-            </button>
-          </div>
-          {/* Remember me checkbox */}
-          <div className='form-check mt-4'>
-            <input
-              className='form-checkbox'
-              type='checkbox'
-              id='rememberMe'
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
-            />
-              <label className='ml-2 text-gray-700' htmlFor='rememberMe'>Remember me</label>
-          </div>
-          {/* Registration link */}
-          <div className='text-center mt-4'>
-            <a className='text-blue-500 hover:text-blue-700' href='/register'>
-              Don't have an account? Register
-            </a>
-          </div>
-        </form>
+    <div className="relative flex items-center justify-center bg-gray-300 py-12 -mb-16 h-[99.1vh]">
+      <div className="absolute inset-0">
+        <Image
+          src="/images/fixedtruck.jpg"
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          className="brightness-[0.4]"
+        />
       </div>
+      <BackgroundGradient className="rounded-[22px] max-w-xl p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <div className="mx-auto w-full max-w-md space-y-6 lg:px-12">
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-gray-500 dark:text-gray-400">Enter your credentials to access your account</p>
+          </div>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="username">Email</Label>
+              <Input id="username" placeholder="Enter your email" />
+            </div>
+            <div className="space-y-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link className="ml-auto text-sm underline" href="#">
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" />
+            </div>
+            <Button className="w-full">Login</Button>
+          </div>
+          <div className="flex justify-center space-x-4">
+            <Button variant="outline">
+              <ChromeIcon className="h-6 w-6" />
+            </Button>
+            <Button variant="outline">
+              <AppleIcon className="h-6 w-6" />
+            </Button>
+            <Button variant="outline">
+              <FacebookIcon className="h-6 w-6" />
+            </Button>
+          </div>
+          <div className="text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link className="underline text-red-600" href="/sign-up">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </BackgroundGradient>
     </div>
-  );
-};
+    
+  )
+}
 
-export default LoginForm;
+function AppleIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" />
+      <path d="M10 2c1 .5 2 2 2 5" />
+    </svg>
+  )
+}
+
+
+function ChromeIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <circle cx="12" cy="12" r="4" />
+      <line x1="21.17" x2="12" y1="8" y2="8" />
+      <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
+      <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
+    </svg>
+  )
+}
+
+
+function FacebookIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+    </svg>
+  )
+}
