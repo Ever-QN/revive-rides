acd95c9d307abb2b0cb20f98bc75f12927dfb368
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-02-29 06:32:08 -0700

    Remove BackgroundGradient import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,6 @@
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { BackgroundGradient } from "@/components/ui/background-gradient"
 import Image from "next/image"
 import Link from "next/link"
 
