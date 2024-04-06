3105bfdc32d5c224cb4e3e5cf4520e64cd3c97be
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-05 22:23:47 -0600

    Edit some words in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -134,12 +134,12 @@ export default function Dashboard() {
           <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
             <div className="flex flex-col items-center gap-1 text-center">
               <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
+                You have no appointments
               </h3>
               <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
+                Appointments will appear here when you have them
               </p>
-              <Button className="mt-4">Add Product</Button>
+              <Button className="mt-4">Book Appointment</Button>
             </div>
           </div>
         </main>
