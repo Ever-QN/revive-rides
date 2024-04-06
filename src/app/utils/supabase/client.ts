0bfcd2955992a7f323b4f9a7e5a0e99d157b154c
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-06 00:32:40 -0600

    Use cookies for auth check instead of localstorage

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -1,8 +1,24 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { type CookieOptions, createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers';
 
 export function createClient() {
-  return createBrowserClient(
+  const cookieStore = cookies();
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+
+          }
+        },
+      },
+    },
   )
 }
