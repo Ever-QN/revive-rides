42793c82000fefcd1c779994322d4c09f3bb35dd
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-07 19:54:19 -0600

    Update UserDropdown.tsx

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -3,6 +3,9 @@ import { createClient } from '@/app/utils/supabase/client';
 import React, { useState, useRef, useEffect } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useRouter } from 'next/navigation';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { CircleUser } from 'lucide-react';
 
 export default function UserDropdown({ user }: any) {
     const supabase = createClient();
@@ -10,74 +13,30 @@ export default function UserDropdown({ user }: any) {
     const userDropdownRef = useRef<HTMLDivElement>(null);
     const router = useRouter();
 
-    
-    const handleClickOutside = (e: any) => {
-        if (userDropdownRef.current && !userDropdownRef.current.contains(e.target as Node)) {
-            setIsOpen(false);
-            return;
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('click', handleClickOutside, true);
-        return () => document.removeEventListener('click', handleClickOutside, true);
-    }, []);
-
     return (
-        <div className="relative">
-            <Avatar
-            className="cursor-pointer"
-            onClick={(e: any) => {
-                setIsOpen(!isOpen);
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-full">
+            <CircleUser className="h-5 w-5" />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="cursor-pointer hover:bg-slate-200">Dashboard</DropdownMenuItem>
+          <DropdownMenuItem 
+          className="cursor-pointer hover:bg-slate-200">Settings</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem 
+          className="cursor-pointer hover:bg-slate-200"
+            onClick={async () => {
+                await supabase.auth.signOut();
+                redirectToPath("/");
             }}
-            
-            >
-                <AvatarImage src="../../../images/avatar.jpg" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-
-        {isOpen && (
-            <div
-            id="userDropdown"
-            className="z-10 absolute top-full left-0 w-44 rounded-lg shadow-md bg-white dark:bg-gray-700 divide-y divide-gray-100 dark:divide-gray-600"
-            ref={userDropdownRef}
-            >
-            <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                <div>Bonnie Green</div>
-                <div className="font-medium truncate">{user.email}</div>
-            </div>
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-                <li>
-                <a href="/dashboard" className="block px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">
-                    Dashboard
-                </a>
-                </li>
-                <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">
-                    Settings
-                </a>
-                </li>
-            </ul>
-            <div className="py-1">
-                <form>
-                <button
-                    onClick={async () => {
-                        const { error } = await supabase.auth.signOut();
-                        if (error) {
-                            console.log(error.message)
-                        } else {
-                            redirectToPath("/home")
-                        }
-                    }}
-                    className="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    
-                > 
-                    Sign out
-                </button>
-                </form>
-            </div>
-            </div>
-        )}
-        </div>
-    );
-}
+          >
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )}
