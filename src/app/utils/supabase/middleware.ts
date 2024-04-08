8d881c8ffb967f672d367715e026f9208427f6d0
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-08 05:26:39 -0600

    Adjust middleware to match up with supabase docs
    To be honest at this point theres like a million different supabase pages on authentication idek if this is the right one

--- src/app/utils/supabase/middleware.ts
+++ src/app/utils/supabase/middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function updateSession(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
@@ -54,7 +54,27 @@ export async function updateSession(request: NextRequest) {
     }
   )
 
-  await supabase.auth.getUser()
+  const {
+    data: { user: isAuthenticated },
+  } = await supabase.auth.getUser();
 
-  return response
+  if (isAuthenticated) {
+    return NextResponse.next();
+  }
+
+  const redirectUrl = request.nextUrl.clone();
+  redirectUrl.pathname = '/';
+  redirectUrl.searchParams.set(`redirectedFrom`, request.nextUrl.pathname);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+  ],
 }
