36bff4139a119a4329732b5ced7d8fea34e7b221
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-08 21:01:19 -0600

    Fix Link href

--- src/components/Navlinks.tsx
+++ src/components/Navlinks.tsx
@@ -44,7 +44,7 @@ export default function Navlinks({ user } : any) {
             {user ? (
                 <UserDropdown user={user}/>
             ) : ( 
-              <Link href="/signin" className='shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear hover:scale-110 active:scale-125 hover:duration-200'>
+              <Link href="/sign-in" className='shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear hover:scale-110 active:scale-125 hover:duration-200'>
                 Login or Sign-Up
               </Link>
             )}
