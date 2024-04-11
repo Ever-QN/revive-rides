cae9d7bbfd2b24769faa89ed63f7878994d49e36
BellaTr <boi.tran@edu.sait.ca>
2024-04-10 19:49:49 -0600

    Update SignIn.test.tsx

--- __test__/SignIn.test.tsx
+++ __test__/SignIn.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, renderHook, act, getByLabelText, fireEvent, getByText } from '@testing-library/react';
+import { z, ZodType } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import userEvent from '@testing-library/user-event';
 import { createClient } from '@/app/utils/supabase/client';
 import SignIn from '@/app/sign-in/page';
 
-
-import { z } from 'zod';
 
 jest.mock('@/app/utils/supabase/client', () => ({
     createClient: jest.fn(() => ({
@@ -18,5 +19,24 @@ describe('SignIn component', () => {
             email: z.string().email(),
             password: z.string(),
         });
-    }); // Add a closing curly brace here
+
+        const { result } = renderHook(() =>
+            useForm({ resolver: zodResolver(schema) })
+        );
+
+        const { formState } = result.current;
+
+        act(() => {
+            result.current.setValue('email', 'test@example.com');
+        });
+        await waitFor(() => {}); 
+        expect(formState.errors.email).toBeUndefined();
+
+        act(() => {
+            result.current.setValue('password', 'password123');
+        });
+        await waitFor(() => {});
+        expect(formState.errors.password).toBeUndefined();
+
+    });
 });
