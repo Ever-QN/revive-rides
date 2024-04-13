76ff9e90c7586e1341d357590b3e21345f26fb61
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-13 00:40:04 -0600

    Create page.tsx

---
+++ src/app/auth/auth-code-success/page.tsx
@@ -0,0 +1,7 @@
+export default function SuccessEmail() {
+    return (
+        <div className='flex w-full justify-center items-center h-screen'>
+            <h1 className='text-green-600 font-bold'>Your email has been successfully verified!</h1>
+        </div>
+    )
+}
