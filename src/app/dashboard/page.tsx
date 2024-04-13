6f5d1359f378fc918021ecc37672f756dbdaf8f2
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-13 00:40:31 -0600

    Fix the client side error with redirectToPath
    Something with tokens idk

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,19 @@
-import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { createClient } from "../utils/supabase/server";
-import { redirectToPath } from "../utils/auth-helpers/server";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
 
     const supabase = createClient();
-    const {
-        data: { user },
-      } = await supabase.auth.getUser();
 
-    if (!user) {
-        return redirectToPath("/sign-in");
-      }
+    const {data : { user }} = await supabase.auth.getUser()
+
+    if (!user) { redirect('/signin') }
+    if (user) { redirect('/dashboard/overview') }
 
     return (
         <div className='flex w-full justify-center items-center h-screen'>
             <h1 className='text-black'>Loading...</h1>
-            {!user && redirectToPath('/sign-in')}
-            {user && redirectToPath('/dashboard/overview')
-            }
         </div>
     )
 }
