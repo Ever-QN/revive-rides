37f4e1df1c61d27a2eb634f4d51534e8a01b1f5b
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-13 02:33:29 -0600

    redirectToPath instead of redirect

--- src/app/dashboard/appointments/page.tsx
+++ src/app/dashboard/appointments/page.tsx
@@ -33,6 +33,7 @@ import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { createClient } from "@/app/utils/supabase/server"
 import UserBookingsTable from "@/components/UserBookingsTable"
+import { redirectToPath } from "@/app/utils/auth-helpers/server"
 
 export default async function DashboardAppointments() {
 
@@ -43,7 +44,7 @@ export default async function DashboardAppointments() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/sign-in");
+    return redirectToPath("/sign-in");
   }
 
   return (
