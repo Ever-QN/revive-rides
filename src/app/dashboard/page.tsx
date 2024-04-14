eb340ea110d5606b4d82640e4175fb71929737fb
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-14 13:34:59 -0600

    Separate header and add dashboard cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,32 +1,61 @@
-'use client'
-
 import { useEffect } from "react";
 import { createClient } from "../utils/supabase/client";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import { Home, LayoutDashboard, LogOut, Users } from "lucide-react";
+import { DashboardOverviewCards } from "@/components/DashboardOverviewCards";
+import DashboardHeader from "@/components/DashboardHeader";
 
 export default function Dashboard() {
 
-  const router = useRouter();
-
-    useEffect(() => {
-      async function checkUser() {
-        const supabase = createClient();
-        const { data: { user } } = await supabase.auth.getUser();
-  
-        if (!user) {
-          router.push("/sign-in");
-        }
-        if (user) {
-          router.push("/dashboard/overview");
-        
-        }
-      }
-      checkUser()
-    }, []);
-
     return (
-        <div className='flex w-full justify-center items-center h-screen'>
-            <h1 className='text-black'>Loading...</h1>
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <LayoutDashboard className="h-6 w-6" />
+              <span className="">S&D Autobody</span>
+            </Link>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="/dashboard"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 bg-muted hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+              <Link
+                href="/dashboard/appointments"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Your Appointments
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LogOut className="h-4 w-4" />
+                Sign Out
+              </Link>
+            </nav>
+          </div>
         </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <div className="flex items-center">
+            <DashboardHeader />
+          </div>
+        </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <DashboardOverviewCards />
+        </main>
+      </div>
+    </div>
+
     )
 }
