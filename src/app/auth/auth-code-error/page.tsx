c3f8a5b5f2d3995606305d4dc73ddb7a6b6fbe40
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-14 12:09:58 -0600

    Add link to go back home

--- src/app/auth/auth-code-error/page.tsx
+++ src/app/auth/auth-code-error/page.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function AuthCodeError() {
     return (
         <div className='flex flex-col w-full justify-center items-center h-screen'>
-            <h1 className='text-red-600 font-bold text-5xl'>The link you tried to visit is invalid or has expired.</h1>
+            <div className='text-red-600 font-bold text-5xl'>The link you tried to visit is invalid or has expired.</div>
+            <div>
+                <Link href="/" className="text-red-500 underline">Click here to go back home</Link>
+            </div>
         </div>
     )
 }
