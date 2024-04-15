25c1dc39e4144ab57e8c1aa8bb681d49ba8765aa
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-14 20:09:46 -0600

    Justify center on mobile for better visual clarity

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -46,12 +46,12 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="flex justify-center md:justify-start h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <div className="flex items-center">
             <DashboardHeader />
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex flex-1 flex-col gap-4 lg:gap-6 lg:p-6">
           <DashboardOverviewCards />
         </main>
       </div>
