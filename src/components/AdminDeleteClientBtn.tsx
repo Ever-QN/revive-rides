1d6bccea0f75b954188b1548a1c0f8e28dc5810b
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2024-04-15 15:57:48 -0600

    Import using @ instead of ./

--- src/components/AdminDeleteClientBtn.tsx
+++ src/components/AdminDeleteClientBtn.tsx
@@ -1,5 +1,5 @@
 import { Trash2 } from "lucide-react";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
 import { createClient } from '@supabase/supabase-js'
