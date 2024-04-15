8ab5b63bf9e7b851e5c7464cde664f60a5b8de8d
Quynh Ngo <71616274+Ever-QN@users.noreply.github.com>
2024-04-15 08:43:48 -0600

    Fix typo

--- src/app/book-appointment/new-appointment/confirm/page.tsx
+++ src/app/book-appointment/new-appointment/confirm/page.tsx
@@ -14,7 +14,7 @@ export default function ConfirmSuccess() {
       <Card className="w-full max-w-md">
         <CardContent className="flex flex-col items-center justify-center p-8">
           <CardTitle className="text-2xl mb-4">Appointment Successfully Booked</CardTitle>
-          <CardDescription className="mb-6">Your appointment as been booked. Once your appointment is confirmed, the status will change from pending to confirmed.</CardDescription>
+          <CardDescription className="mb-6">Your appointment has been booked. Once your appointment is confirmed, the status will change from pending to confirmed.</CardDescription>
           <Button onClick={handleDashboardRedirect} className="mt-4">
             Go to Dashboard
           </Button>
